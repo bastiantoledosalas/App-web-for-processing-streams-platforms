004006e523a2cd259a360b3d3eff529311c8596e
bastiantoledosalas <toledosalasbastian@gmail.com>
2024-11-15 16:52:12 -0300

    Eliminando archivos innecesario

--- Codigo de Prueba/Project-Backend 2/src/Contexts/Shared/domain/EventBus.ts
+++
@@ -1,7 +0,0 @@
-import { type DomainEventSubscribers } from '../infrastructure/EventBus/DomainEventSubscribers'
-import { type DomainEvent } from './DomainEvent'
-
-export interface EventBus {
-  publish: (events: DomainEvent[]) => Promise<void>
-  addSubscribers: (subscribers: DomainEventSubscribers) => void
-}
--- Codigo de Prueba/Project-Backend 2/src/Contexts/Shared/domain/QueryBus.ts
+++
@@ -1,6 +0,0 @@
-import { type Query } from './Query'
-import { type Response } from './Response'
-
-export interface QueryBus {
-  ask: <R extends Response>(query: Query) => Promise<R>
-}
